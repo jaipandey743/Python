2467de740a59a473791957f311f4bef48476fc0b
Jai Pandey <jaipandey743@gmail.com>
2026-04-05 19:18:38 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,25 @@
+#Arithmatic Operator
+a=13
+b=15
+c=a+b
+print(c)
+
+# Assignment Operator
+a=7
+print(a)
+b=8
+b+=3 # Incrementing the value of b by 3 and then assigning it to b
+print(b)
+
+# Comparison Operator
+a=7>9
+print(a) # Comparison Operator return boolean value (True or False)
+
+# Logical Operators
+
+e= True or False
+print(e) 
+
+
+
+
