bca44980de2da2c902e020f8da451ef350d21a98
Jai Pandey <jaipandey743@gmail.com>
2026-04-12 12:38:12 +0530

    string functions

---
+++ basics/str_functions.py
@@ -0,0 +1,9 @@
+# This function returns the length of the strings.
+name="jerry"
+print(len(name))
+# Capitalizes the string
+print(name.capitalize())
+# Checks if the string ends with rry or not
+print(name.endswith("rry"))
+# Checks if the string starts with jer or not
+print(name.startswith("jr"))
