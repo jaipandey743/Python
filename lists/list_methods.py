9a0a02c0261386e116d7b52e25281eb189ad54a3
Jai Pandey <jaipandey743@gmail.com>
2026-04-15 20:28:13 +0530

    list methods

---
+++ lists/list_methods.py
@@ -0,0 +1,9 @@
+friends= [1,"lucia","jason","arthur","trevor"]
+print(friends)
+friends.append("nico")
+print(friends)
+l1 = [2,55,24,25,18]
+l1.sort
+print(l1)
+l1.insert(1,23) # insert 23 in the 2nd position
+print(l1)
