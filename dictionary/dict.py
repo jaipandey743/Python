cc913403adde030a91ebe840c72f123da56a06c8
Jai Pandey <jaipandey743@gmail.com>
2026-04-18 08:23:57 +0530

    dictionary

---
+++ dictionary/dict.py
@@ -0,0 +1,13 @@
+marks={
+    "Maths":100,
+    "English":99,
+    "Science":98,
+    "Hindi":97,
+    "Computer":96
+}
+print(type(marks))
+print(marks["Maths"])
+# python dictionaries are mutable
+# they are unordered
+# they are indexed
+# cannot contain duplicate keys
