c90ddfc5cd6554766eee154251246f368c4dbbe2
Jai Pandey <jaipandey743@gmail.com>
2026-04-19 10:38:42 +0530

    sets

---
+++ sets/sets.py
@@ -0,0 +1,5 @@
+e= set() # dont use  s = {} as it will create an empty dictionary instead of empty set
+a={5,17,"hello",5,9,9,3} # set is different from dictionary because it dosent repeadt elements
+print(a)
+print(type(a))
+print(e)
