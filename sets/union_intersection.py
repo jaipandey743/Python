80998d6f95533fe7c405af0d0108442aef715d36
Jai Pandey <jaipandey743@gmail.com>
2026-04-19 18:00:32 +0530

    union intersections

---
+++ sets/union_intersection.py
@@ -0,0 +1,6 @@
+s1={5,17,"hello",5,9,31,3}
+s2={18,20,"hi",19,4,112,90}
+print(s1.union(s2)) # combines both sets
+print(s1.intersection(s2)) # gives common elements
+# I have wrote these set methods in different file because they are important
+
