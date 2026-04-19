701377088fbbaced42139a6f7536b08eef882fb2
Jai Pandey <jaipandey743@gmail.com>
2026-04-19 18:05:10 +0530

    set methods

---
+++ sets/set_methods.py
@@ -0,0 +1,9 @@
+a={5,17,"hello",5,9,9,3}
+print(a,type(a))
+a.add("Jai") # adds a new element
+# elements order dosent matter
+# cannot access elements index
+# there is now way to change items in sets
+# sets cannot contain duplicate values
+a.remove("hello") # removes the element
+print(a)
